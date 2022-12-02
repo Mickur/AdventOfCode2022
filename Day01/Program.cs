607ba1131598adc91d38bd50db1d00c423a16ce6
Mickur <38385999+Mickur@users.noreply.github.com>
2022-12-02 02:51:24 +0100

    Gotta go FASTER!

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,15 +4,36 @@ Console.WriteLine("Mickur's Advent of Code 2022 - Day 1!");
 
 // Setup
 var stopWatch = new Stopwatch();
-var elves = new List<int>();
+
 var currentElfCalories = 0;
+
+var gold = 0;
+var silver = 0;
+var bronze = 0;
 
 stopWatch.Start();
 
 foreach (var line in File.ReadLines("input.txt"))
     if (string.IsNullOrWhiteSpace(line))
     {
-        elves.Add(currentElfCalories);
+        // Are we top 3!?
+        if (currentElfCalories > bronze)
+        {
+            bronze = currentElfCalories;
+            
+            if (currentElfCalories >= silver)
+            {
+                bronze = silver;
+                silver = currentElfCalories;
+                
+                if (currentElfCalories >= gold)
+                {
+                    silver = gold;
+                    gold = currentElfCalories;
+                }
+            }
+        }
+        
         currentElfCalories = 0;
     }
     else
@@ -28,38 +49,15 @@ foreach (var line in File.ReadLines("input.txt"))
         currentElfCalories += lineValue;
     }
 
-if (currentElfCalories > 0)
-    elves.Add(currentElfCalories);
-
 stopWatch.Stop();
 Console.WriteLine($"Parsed input in {stopWatch.ElapsedMilliseconds} ms ({stopWatch.ElapsedTicks:n0} ticks)");
 stopWatch.Restart();
 
-// Note: There are better ways to get the top elves than this. But I don't care, this code is simple! I also assume that the input is correctly formatted and that there are always at least three elves.
-elves.Sort((a, b) =>
-{
-    if (a < b)
-    {
-        return 1;
-    }
-    
-    if (a > b)
-    {
-        return -1;
-    }
-    
-    return 0;
-});
-
-stopWatch.Stop();
-Console.WriteLine($"Sorted elves in {stopWatch.ElapsedMilliseconds} ms ({stopWatch.ElapsedTicks:n0} ticks)");
-stopWatch.Restart();
-
 // Part One: Find elf carrying the most
-Console.WriteLine($"Part 1: The elf carrying the most calories has {elves[0]} calories");
+Console.WriteLine($"Part 1: The elf carrying the most calories has {gold} calories");
 
 // Part Two: Find top 3 elves carrying the most
-Console.WriteLine($"Part 2: The three elves that are carrying the most calories have {elves[0] + elves[1] + elves[2]} calories total");
+Console.WriteLine($"Part 2: The three elves that are carrying the most calories have {gold + silver + bronze} calories total");
 
 stopWatch.Stop();
 Console.WriteLine($"Printed answers in {stopWatch.ElapsedMilliseconds} ms ({stopWatch.ElapsedTicks:n0} ticks)");
