596a4357d152e0c39395495dca20a046dd188b24
Mickur <38385999+Mickur@users.noreply.github.com>
2022-12-06 18:03:59 +0100

    Added some black magic, and now we're going super fast!

--- BenchmarkSandbox/Program.cs
+++ BenchmarkSandbox/Program.cs
@@ -13,19 +13,19 @@ namespace BenchmarkSandbox
     }
 
     [MemoryDiagnoser]
-    [SimpleJob(RuntimeMoniker.Net60, baseline: true)]
+    //[SimpleJob(RuntimeMoniker.Net60, baseline: true)]
     [SimpleJob(RuntimeMoniker.Net70)]
     public class Day6
     {
-        private string[] input;
+        private string input;
 
-        [Params(4, 14)]
+        [Params(4, 14, 16)]
         public int neededUniquesInARow;
 
         [GlobalSetup]
         public void Setup()
         {
-            input = File.ReadAllLines("input.txt");
+            input = File.ReadAllLines("inputs/2022-Day06.txt")[0];
         }
 
         [Benchmark]
@@ -33,18 +33,51 @@ namespace BenchmarkSandbox
         {
             var uniqueCheckHashSet = new HashSet<char>(neededUniquesInARow);
 
-            for (var i = 0; i < input[0].Length; i++)
+            for (var i = 0; i < input.Length; i++)
             {
                 uniqueCheckHashSet.Clear();
                 for (var j = 0; j < neededUniquesInARow; j++)
                 {
-                    if (i + j >= input[0].Length || uniqueCheckHashSet.Add(input[0][i + j]) == false)
+                    if (i + j >= input.Length || uniqueCheckHashSet.Add(input[i + j]) == false)
                         break;
 
                     if (j == neededUniquesInARow - 1)
                     {
                         return i + neededUniquesInARow;
                     }
+                }
+            }
+
+            return -1;
+        }
+        
+        [Benchmark]
+        public int BitSolution()
+        {
+            for (var i = 0; i < input.Length - neededUniquesInARow; i++)
+            {
+                uint charBits = 0;
+                
+                for (var j = 0; j < neededUniquesInARow; j++)
+                {
+                    // Operators
+                    // & - Apply mask
+                    // | - Combine
+                    // ^ - Combine/Toggle, get difference
+                    
+                    // Convert char to an int value and push it to the correct position in the mask
+                    uint charMask = (uint)1 << (input[i+j] - 'a');
+                    
+                    // Checks if the value has already been seen
+                    if ((charBits & charMask) > 0)
+                        break;
+                    
+                    // Add value to the ones we've seen
+                    charBits |= charMask;
+                    
+                    // If we got this far in this loop, we're successful!
+                    if (j == neededUniquesInARow - 1)
+                        return i + neededUniquesInARow;
                 }
             }
 
--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,25 +1,43 @@
 ﻿Console.WriteLine("Mickur's Advent of Code 2022 - Day 6!");
 
 // Setup
-var input = File.ReadAllLines("input.txt");
+var input = File.ReadAllLines("input.txt")[0];
 const int partOneUniquesNeeded = 4;
 const int partTwoUniquesNeeded = 14;
 const int maxUniquesNeeded = partTwoUniquesNeeded > partOneUniquesNeeded ? partTwoUniquesNeeded : partOneUniquesNeeded;
 
-var uniqueCheckHashSet = new HashSet<char>(maxUniquesNeeded);
 var partOneAllUniqueFound = false;
 var partOneAnswer = -1;
 var partTwoAllUniqueFound = false;
 var partTwoAnswer = -1;
 
-for (var i = 0; i < input[0].Length; i++)
+for (var i = 0; i < input.Length; i++)
 {
-    uniqueCheckHashSet.Clear();
+    if (partOneAllUniqueFound && partTwoAllUniqueFound)
+        break;
+    
+    uint charBits = 0;
+                
     for (var j = 0; j < maxUniquesNeeded; j++)
     {
-        if (i + j >= input[0].Length || uniqueCheckHashSet.Add(input[0][i + j]) == false)
+        if (i + j >= input.Length)
             break;
-
+        
+        // Operators
+        // & - Apply mask
+        // | - Combine
+        // ^ - Combine/Toggle, get difference
+                    
+        // Convert char to an int value and push it to the correct position in the mask
+        uint charMask = (uint)1 << (input[i+j] - 'a');
+                    
+        // Checks if the value has already been seen
+        if ((charBits & charMask) > 0)
+            break;
+                    
+        // Add value to the ones we've seen
+        charBits |= charMask;
+        
         if (!partOneAllUniqueFound && j == partOneUniquesNeeded - 1)
         {
             partOneAnswer = i + partOneUniquesNeeded;
@@ -32,9 +50,6 @@ for (var i = 0; i < input[0].Length; i++)
             partTwoAllUniqueFound = true;
         }
     }
-    
-    if(partOneAllUniqueFound && partTwoAllUniqueFound)
-        break;
 }
 
 Console.WriteLine($"Answer to part one: {partOneAnswer}");
