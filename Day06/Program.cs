f6de9b6992fcde77339b2ecc7c2ab765157a3ccd
Mickur <38385999+Mickur@users.noreply.github.com>
2022-12-06 07:44:30 +0100

    JK, let's combine the loops!

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -6,53 +6,38 @@ Console.WriteLine("Mickur's Advent of Code 2022 - Day 6!");
 var input = File.ReadAllLines("input.txt");
 const int partOneUniquesNeeded = 4;
 const int partTwoUniquesNeeded = 14;
+const int maxUniquesNeeded = partTwoUniquesNeeded > partOneUniquesNeeded ? partTwoUniquesNeeded : partOneUniquesNeeded;
 
 var sw = new Stopwatch();
 sw.Start();
 
-var queue = new Queue<char>();
 var uniqueCheckHashSet = new HashSet<char>();
-var allUnique = false;
+var partOneAllUniqueFound = false;
+var partTwoAllUniqueFound = false;
 
 for (var i = 0; i < input[0].Length; i++)
 {
     uniqueCheckHashSet.Clear();
-    for (var j = 0; j < partOneUniquesNeeded; j++)
+    for (var j = 0; j < maxUniquesNeeded; j++)
     {
         if (uniqueCheckHashSet.Add(input[0][i + j]) == false)
             break;
 
-        if (j == partOneUniquesNeeded - 1)
-            allUnique = true;
-    }
+        if (!partOneAllUniqueFound && j == partOneUniquesNeeded - 1)
+        {
+            Console.WriteLine($"Answer to part one: {i + partOneUniquesNeeded}");
+            partOneAllUniqueFound = true;
+        }
 
-    if (allUnique)
-    {
-        Console.WriteLine($"Answer to part one: {i + partOneUniquesNeeded}");
+        if (!partTwoAllUniqueFound && j == partTwoUniquesNeeded - 1)
+        {
+            Console.WriteLine($"Answer to part two: {i + partTwoUniquesNeeded}");
+            partTwoAllUniqueFound = true;
+        }
+    }
+    
+    if(partOneAllUniqueFound && partTwoAllUniqueFound)
         break;
-    }
-}
-
-queue.Clear();
-allUnique = false;
-
-for (var i = 0; i < input[0].Length; i++)
-{
-    uniqueCheckHashSet.Clear();
-    for (var j = 0; j < partTwoUniquesNeeded; j++)
-    {
-        if (uniqueCheckHashSet.Add(input[0][i + j]) == false)
-            break;
-
-        if (j == partTwoUniquesNeeded - 1)
-            allUnique = true;
-    }
-
-    if (allUnique)
-    {
-        Console.WriteLine($"Answer to part two: {i + partTwoUniquesNeeded}");
-        break;
-    }
 }
 
 sw.Stop();
